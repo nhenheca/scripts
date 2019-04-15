5bc4fad2dab37c95fd4cf28509bc820ed4689eed
nhenheca <33757448+nhenheca@users.noreply.github.com>
2019-04-15 08:57:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-﻿    console.log("Se quiser associar novos edificios faça scroll down e click no botão RESET: localStorage.removeItem(\"reload\"); ");
+﻿   // ==UserScript==
+// @name         ConstrutorAFK
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  ConstrutorAFK
+// @author       nheneheca
+// @match        https://pt66.tribalwars.com.pt/game.php?village=17034&screen=main
+// @grant        none
+// ==/UserScript==
+
+    console.log("Se quiser associar novos edificios faça scroll down e click no botão RESET: localStorage.removeItem(\"reload\"); ");
 
     /*butao----------------*/
-    var btn = document.createElement("BUTTON");
+    var btn = document.createElement("button");
     btn.innerHTML = "RESET";
     document.body.appendChild(btn);
     btn.onclick = function () {
@@ -12,8 +22,8 @@
     if(localStorage.getItem("reload")!="t"){
         var arrayEdificiosNiveis = [];
 
-        console.log("Edificio Principal - Main\nQuartel - barracks\nIgreija - church_f\nFerreiro - smith\nPraça - place\nEstátua - statue\nMercado - market\nBosque - wood\nPoço - stone\nMina - iron\nFazenda - farm\nArmazem - storage\nEsconderijo - hide\nMuralha - wall\nEstabulo - stable\nOficina - garage\nTorre de Vigia - watchtower\nAcademia - snob\n");
         alert("Pressione: Ctrl+Shift+J");
+        console.log("Edificio Principal - Main\nQuartel - barracks\nIgreija - church_f\nFerreiro - smith\nPraça - place\nEstátua - statue\nMercado - market\nBosque - wood\nPoço - stone\nMina - iron\nFazenda - farm\nArmazem - storage\nEsconderijo - hide\nMuralha - wall\nEstabulo - stable\nOficina - garage\nTorre de Vigia - watchtower\nAcademia - snob\n");
 
         var edificiosAux = prompt("Digite o nome dos Edificios", "ex: iron wood stone market");
         var arrayEdificios = edificiosAux.split(" ");
@@ -36,13 +46,19 @@
         }
 
         var indexMenor = arrayEdificiosNiveis1.indexOf(Math.min.apply(null, arrayEdificiosNiveis1));
-        console.log(indexMenor);
         var butao = document.getElementById("main_buildrow_"+arrayEdificios1[indexMenor]).children[6].children[1];
         if(butao.style.display != "none"){
             butao.click();
+        }else{
+            for(let i=0;i<arrayEdificios1.length;i++){
+                butao = document.getElementById("main_buildrow_"+arrayEdificios1[i]).children[6].children[1];
+                if(butao.style.display != "none"){
+                    butao.click();
+                }
+            }
         }
     }
-console.log(arrayEdificiosNiveis1,arrayEdificios1);
+    console.log(arrayEdificiosNiveis1,arrayEdificios1);
     window.setInterval(function(){
         localStorage.setItem("reload", "t");
         myFunction();
